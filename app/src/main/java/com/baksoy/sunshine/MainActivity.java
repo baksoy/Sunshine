b837815af22712752a75a700954ab2164559c126
baksoy <baksoy@gmail.com>
2015-06-10 19:31:34 -0400

    Add Dummy Weather Forecast

--- app/src/main/java/com/baksoy/sunshine/MainActivity.java
+++ app/src/main/java/com/baksoy/sunshine/MainActivity.java
@@ -1,7 +1,7 @@
 package com.baksoy.sunshine;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -14,7 +14,6 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -22,11 +21,12 @@ public class MainActivity extends ActionBarActivity {
         return true;
     }
 
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        // as you specify a parent activity in AndroidManifest.xml
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -37,3 +37,4 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
--- app/src/main/java/com/baksoy/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/baksoy/sunshine/MainActivityFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 
 /**
  * A placeholder fragment containing a simple view.
@@ -18,6 +20,15 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ArrayList<String> weatherForecast = new ArrayList<String>();
+        weatherForecast.add("Sun, Jun 1 - Clear - 31/17");
+        weatherForecast.add("Mon, Jun 2 - Clear - 21/14");
+        weatherForecast.add("Tue, Jun 3 - Cloudy - 22/17");
+        weatherForecast.add("Wed, Jun 4 - Rainy - 18/11");
+        weatherForecast.add("Fri, Jun 6 - Rainy - 27/15");
+
+        return rootView;
     }
 }
