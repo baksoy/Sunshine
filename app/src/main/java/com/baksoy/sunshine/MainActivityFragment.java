a02f30eb41c3011df6e8a233e628ac6aba92fb38
baksoy <baksoy@gmail.com>
2015-06-13 22:41:22 -0400

    Lesson 1 Final Commit

--- app/src/main/java/com/baksoy/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/baksoy/sunshine/MainActivityFragment.java
@@ -5,13 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import java.util.ArrayList;
 
-
-/**
- * A placeholder fragment containing a simple view.
- */
 public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {
@@ -22,12 +19,17 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        ArrayList<String> weatherForecast = new ArrayList<String>();
-        weatherForecast.add("Sun, Jun 1 - Clear - 31/17");
-        weatherForecast.add("Mon, Jun 2 - Clear - 21/14");
-        weatherForecast.add("Tue, Jun 3 - Cloudy - 22/17");
-        weatherForecast.add("Wed, Jun 4 - Rainy - 18/11");
-        weatherForecast.add("Fri, Jun 6 - Rainy - 27/15");
+
+        String[] forecastArray = {"Sun, Jun 1 - Clear - 31/17", "Mon, Jun 2 - Clear - 21/14",
+                "Tue, Jun 3 - Cloudy - 22/17", "Wed, Jun 4 - Rainy - 18/11", "Fri, Jun 6 - Rainy - 27/15"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, forecastArray);
+
+        //Finding our AdapterView type (ListView in this case) within the root container we declared above
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+
+        //Setting our AdapterView to display the array within our adapter
+        listView.setAdapter(adapter);
 
         return rootView;
     }
