fd7abfc9389bc99737fe3c7ffcf62572a2e737d3
baksoy <baksoy@gmail.com>
2015-06-28 16:29:53 -0400

    Fix crash

--- app/src/main/java/com/baksoy/sunshine/SettingsActivity.java
+++ app/src/main/java/com/baksoy/sunshine/SettingsActivity.java
@@ -22,7 +22,6 @@ public class SettingsActivity extends PreferenceActivity
         super.onCreate(savedInstanceState);
         // Add 'general' preferences, defined in the XML file
         // TODO: Add preferences from XML
-        addPreferencesFromResource(R.xml.pref_general);
 
         // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
         // updated when the preference changes.
