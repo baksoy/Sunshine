786636000e3f98c8298134a8a1dd308644bd774f
baksoy <baksoy@gmail.com>
2015-06-28 16:06:22 -0400

    Show forecast detail Activity when you click on a day

--- app/src/main/java/com/baksoy/sunshine/DetailActivity.java
+++ app/src/main/java/com/baksoy/sunshine/DetailActivity.java
@@ -42,8 +42,7 @@ public class DetailActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            Intent intent = new Intent(this, SettingsActivity.class);
-            startActivity(intent);
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
